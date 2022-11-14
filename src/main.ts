0ff9d39ecc7223d0e3a032f3b0c68d4ec9021e97
Lucas Chapuis <l.chapuis@patriarche.fr>
2022-11-14 10:57:34 +0100

    feat: move router to plugins

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import 'normalize.css';
 import 'github-markdown-css';
 import '@/assets/scss/main.scss';
 import i18n from '@/plugins/i18n';
+import router from '@/plugins/router';
 import '@/lib/modules/i18n';
 
 import App from '@/App.vue';
-import router from '@/router';
 
 const app = createApp(App);
 
