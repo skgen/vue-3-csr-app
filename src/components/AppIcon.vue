86d31580820af22c655b5b7760b7d25f60ed3e17
Lucas Chapuis <l.chapuis@patriarche.fr>
2022-11-14 10:56:28 +0100

    feat: add base icon component from material symbols

---
+++ src/components/AppIcon.vue
@@ -0,0 +1,41 @@
+<template>
+  <span
+    class="pux-AppIcon"
+    :data-fill="props.fill || undefined"
+  >
+    {{ props.icon }}
+  </span>
+</template>
+
+<script lang="ts" setup>
+
+type Props = {
+  icon: string;
+  fill?: boolean;
+};
+
+const props = defineProps<Props>();
+</script>
+
+<style lang="scss">
+.pux-AppIcon {
+    display: inline-block;
+    font-family: "Material Symbols Outlined", sans-serif;
+    font-size: inherit;
+    font-style: normal;
+    font-weight: normal;
+    font-feature-settings: "liga";
+    line-height: 1;
+    color: currentcolor;
+    text-transform: none;
+    letter-spacing: normal;
+    word-wrap: normal;
+    white-space: nowrap;
+    direction: ltr;
+    -webkit-font-smoothing: antialiased;
+
+    &[data-fill="true"] {
+        font-variation-settings: "FILL" 1;
+    }
+}
+</style>
